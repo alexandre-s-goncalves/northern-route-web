3e5c723b9fac322a2cab0360f35dcec617e882ae
Alexandre Gonçalves <alexandre.sgoncalves@outlook.com>
2026-08-25 15:01:21 -0300

    chore(ci): enforce explicit env block declaration pattern for compile time variables Ingestion (#15)

--- src/infrastructure/api/apiClient.ts
+++ src/infrastructure/api/apiClient.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
 import { authInterceptor } from './authInterceptor';
 
-const baseURL = import.meta.env.VITE_API_URL;
-
 export const apiClient = axios.create({
-  baseURL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
