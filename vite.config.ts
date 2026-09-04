0bbcdf0214ad81c5a44f837e1a9bc615d1a6701d
Alexandre <alexandre.sgoncalves@outlook.com>
2026-09-04 19:17:37 -0300

    fix(web): return object literal pattern to vite config to fix vitest config merging exceptions

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,28 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig(({ mode }) => {
-  const isQa = mode === 'qa' || mode === 'staging';
-  const baseFolder = isQa ? '/northern-route-web/qas/' : '/northern-route-web/';
+const isQaBuild =
+  process.env.NODE_ENV === 'qa' || process.env.NODE_ENV === 'staging';
+const baseFolder = isQaBuild
+  ? '/northern-route-web/qas/'
+  : '/northern-route-web/';
 
-  return {
-    base: baseFolder,
-    plugins: [
-      react(),
-      svgr({
-        svgrOptions: {
-          icon: true,
-          exportType: 'default',
-        },
-      }),
-    ],
-    resolve: {
-      tsconfigPaths: true,
-    },
-    server: {
-      port: 5173,
-      open: true,
-    },
-  };
+export default defineConfig({
+  base: baseFolder,
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        exportType: 'default',
+      },
+    }),
+  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
 });
